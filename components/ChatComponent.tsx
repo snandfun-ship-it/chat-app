53af21e191c8a9e6137adfd549652905e82c431e
snandfun-ship-it <snandfun@gmail.com>
2025-11-10 00:33:50 +0530

    first commit

--- components/ChatComponent.tsx
+++ components/ChatComponent.tsx
@@ -14,7 +14,6 @@ import {
 import type { StreamChat } from 'stream-chat';
 import 'stream-chat-react/dist/css/v2/index.css';
 import { useSearchParams } from 'next/navigation';
-import { EmojiPicker } from "stream-chat-react/emojis";
 
 const apiKey = 'cnfe3kpxhv5h';
 
@@ -59,7 +58,7 @@ export default function PrivateChat(): JSX.Element {
     <div className="h-screen xl:bg-linear-to-br bg-black from-indigo-900 via-purple-900 to-black text-white flex items-center justify-center">
       <div className="w-full xl:max-w-3xl h-full bg-black/40 backdrop-blur-md xl:rounded-2xl shadow-xl overflow-hidden border border-white/10">
         <Chat client={client} theme="str-chat__theme-dark">
-          <Channel EmojiPicker={EmojiPicker} channel={channel}>
+          <Channel channel={channel}>
             <Window>
               <ChannelHeader image='https://instagram.fixc4-3.fna.fbcdn.net/v/t1.15752-9/566492912_1162821315239215_8611799929617078573_n.png?stp=cp0_dst-png&_nc_cat=109&ccb=1-7&_nc_sid=0024fc&_nc_ohc=iqSvgYk_XeEQ7kNvwEblXmQ&_nc_oc=AdlQK8zcC9I9BH7jbKVbTJi_K4h2Z8lyiIBfgaDsMLHKBRv_S6iikMQOfgUjd0P6U28&_nc_zt=23&_nc_ht=instagram.fixc4-3.fna&oh=03_Q7cD3wHNoEF7MVCPAOyCnJqfwvSwBLRgDwFLO0L1fOXKXZhY9w&oe=693851B0' title="💞 Jaskarandeep ❤️ Harsimrat 💞" />
               <MessageList />
